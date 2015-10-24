73a24ba43225974ed7d1e97299ec64a1c3735c2a
Christian Fei <crifei93@gmail.com>
2015-10-24 22:35:33 +0200

    added router test

--- test/hello_plug_test.exs
+++ test/hello_plug_test.exs
@@ -1,8 +1,17 @@
 defmodule HelloPlugTest do
-  use ExUnit.Case
-  doctest HelloPlug
+  use ExUnit.Case, async: true
+  use Plug.Test
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  alias HelloPlug.Router
+
+  @opts Router.init([])
+
+  test "returns hello world" do
+    conn = conn(:get, "/")
+    conn = Router.call(conn, @opts)
+
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == "Hello Plug!"
   end
 end
